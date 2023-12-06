1767f79a130c2bbeee6c794754032002fda8eb5b
ElnurKoke <akezanpprynbek@gmail.com>
2023-12-06 23:58:38 +0300

    fo

---
+++ forum/internal/handler/homepage.go
@@ -0,0 +1,77 @@
+package handler
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"net/http"
+	"sort"
+
+	"forum/internal/models"
+)
+
+type ByCreatedAt []models.Post
+
+func (a ByCreatedAt) Len() int           { return len(a) }
+func (a ByCreatedAt) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByCreatedAt) Less(i, j int) bool { return a[i].CreateAt.After(a[j].CreateAt) }
+
+func (h *Handler) homePage(w http.ResponseWriter, r *http.Request) {
+	var posts []models.Post
+	if r.URL.Path != "/" {
+		h.ErrorPage(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	user := r.Context().Value("user").(models.User)
+	if r.Method != http.MethodGet {
+		h.ErrorPage(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	categories, err := h.Service.GetCategories()
+	if err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			h.ErrorPage(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+	if r.URL.Query().Has("category") {
+		category := r.URL.Query().Get("category")
+		if !inSlice(category, categories) {
+			h.ErrorPage(w, "Not exist", http.StatusBadRequest)
+		}
+		posts, err = h.Service.ServicePostIR.GetAllPostsByCategories(category)
+		if err != nil {
+			if !errors.Is(err, sql.ErrNoRows) {
+				h.ErrorPage(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	} else {
+		posts, err = h.Service.ServicePostIR.GetAllPosts()
+		if err != nil {
+			if !errors.Is(err, sql.ErrNoRows) {
+				h.ErrorPage(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+	sort.Sort(ByCreatedAt(posts))
+	info := models.InfoPosts{
+		user,
+		posts,
+		categories,
+	}
+	if err := h.Temp.ExecuteTemplate(w, "homepage.html", info); err != nil {
+		log.Println(err.Error())
+		h.ErrorPage(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func inSlice(val string, slice []string) bool {
+	for _, item := range slice {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
--- testerCss/go.mod
+++
@@ -1,9 +0,0 @@
-module forum
-
-go 1.20
-
-require (
-	github.com/google/uuid v1.3.1 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	golang.org/x/crypto v0.13.0 // indirect
-)
